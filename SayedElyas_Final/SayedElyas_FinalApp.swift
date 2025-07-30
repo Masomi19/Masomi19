20e71a1bd1957abddcc656a25a5d350a5435a45d
Masomi19 <95808378+Masomi19@users.noreply.github.com>
2025-07-30 14:10:07 -0400

    Add files via upload

---
+++ SayedElyas_Final/SayedElyas_FinalApp.swift
@@ -0,0 +1,18 @@
+//
+//  SayedElyas_FinalApp.swift
+//  SayedElyas_Final
+//
+//  Created by Sayed  Elyas Masomi on 2025-06-09.
+//
+
+import SwiftUI
+
+@main
+struct SayedElyas_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+//            ContentView()
+            PackageListView()
+        }
+    }
+}
